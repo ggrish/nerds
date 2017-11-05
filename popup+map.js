880ce66f1e424ea1c75f76853c0919762f800632
ggrish <g.gubarev63@gmail.com>
2017-11-05 22:51:03 +0400

    Add files via upload

--- popup+map.js
+++ popup+map.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mapOpen = document.querySelector(".js-open-map");
 var mapPopup = document.querySelector(".modal-content-map");
 var Overlay = document.querySelector(".modal-overlay");
@@ -53,59 +52,3 @@ mapClose.addEventListener("click", function(event) {
       }
     }
   });
-=======
-var mapOpen = document.querySelector(".js-open-map");
-var mapPopup = document.querySelector(".modal-content-map");
-var Overlay = document.querySelector(".modal-overlay");
-var mapClose = mapPopup.querySelector(".modal-content-close");
-var link = document.querySelector(".btn-popup");
-var popup = document.querySelector(".modal-content");
-var form = popup.querySelector("form");
-var close = document.querySelector(".modal-content-close1");
-
-mapOpen.addEventListener("click", function(event) {
-  event.preventDefault();
-  mapPopup.classList.add("modal-content-show");
-  document.getElementById("modal-overlay").style.display = "block";
-});
-mapClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  mapPopup.classList.remove("modal-content-show");
-  document.getElementById("modal-overlay").style.display = "none";
-
-  });
-  window.addEventListener("keydown", function (event) {
-    if (event.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-content-show")) {
-      mapPopup.classList.remove("modal-content-show");
-      document.getElementById("modal-overlay").style.display = "none";
-      }
-    }
-  });
-
-  link.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.add("modal-content-show");
-  });
-  close.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("modal-content-show");
-    popup.classList.remove("modal-error");
-    });
-    form.addEventListener("submit", function(event) {
-      if (!login.value || !password.value) {
-        event.preventDefault();
-        popup.classList.add("modal-error");
-      }  else {
-        localStorage.setItem("login", login.value);
-      }
-  });
-  window.addEventListener("keydown", function (event) {
-    if (event.keyCode === 27) {
-      if (popup.classList.contains("modal-content-show")) {
-      popup.classList.remove("modal-content-show");
-      popup.classList.remove("modal-error");
-      }
-    }
-  });
->>>>>>> a5f168255655d691cd4a633a10586246a54ed877
